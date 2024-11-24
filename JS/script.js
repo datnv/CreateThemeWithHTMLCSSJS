bd89aa2bb226c5c1dcda9d254a1a799c29fcb512
datnv <datnv030691@gmail.com>
2024-11-24 19:24:00 +0100

    build project section

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,37 @@
+// navbar
 const navbar = document.querySelector(".navbar");
 
 window.addEventListener("scroll", () => {
-    if(window.scrollY > 0) {
-        navbar.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky");
-    }
+  if (window.scrollY > 0) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
 });
+// End of navbar
+
+// Projects
+const filterLinks = document.querySelectorAll(".filter-nav-link");
+
+filterLinks.forEach((filterLink) => {
+  filterLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    document
+      .querySelector(".filter-nav-link.active")
+      .classList.remove("active");
+    filterLink.classList.add("active");
+
+    const projects = document.querySelectorAll(".project");
+    projects.forEach((project) => {
+      project.classList.add("hide");
+      if (
+        filterLink.getAttribute("data-type") ===
+          project.getAttribute("data-type") ||
+        filterLink.getAttribute("data-type") === "all"
+      ) {
+        project.classList.remove("hide");
+      }
+    });
+  });
+});
+// End of Projects
